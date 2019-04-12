cd455b2527fdf3f3e47bac70b5637d5ffff9240a
Dylan Davis <djdavis1420@gmail.com>
2019-04-12 17:00:52 -0500

    Exercises from Java Course

---
+++ src/ClassExercises/Circle.java
@@ -0,0 +1,18 @@
+package ClassExercises;
+
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = (radius > 0) ? radius : 0;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public double getArea() {
+        return (this.radius * this.radius) * Math.PI;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -89,21 +89,44 @@ public class Main {
 //        floor = new Floor(5.4, 4.5);
 //        calculator = new Calculator(floor, carpet);
 //        System.out.println("total= " + calculator.getTotalCost());
-
-        // ClassExercises.ComplexOperations
-        System.out.println("-----ClassExercises.ComplexOperations-----");
-        ComplexNumber one = new ComplexNumber(1.0, 1.0);
-        ComplexNumber number = new ComplexNumber(2.5, -1.5);
-        one.add(1,1);
-        System.out.println("one.real= " + one.getReal());
-        System.out.println("one.imaginary= " + one.getImaginary());
-        one.subtract(number);
-        System.out.println("one.real= " + one.getReal());
-        System.out.println("one.imaginary= " + one.getImaginary());
-        number.subtract(one);
-        System.out.println("number.real= " + number.getReal());
-        System.out.println("number.imaginary= " + number.getImaginary());
-
+//
+//        // ClassExercises.ComplexOperations
+//        System.out.println("-----ClassExercises.ComplexOperations-----");
+//        ComplexNumber one = new ComplexNumber(1.0, 1.0);
+//        ComplexNumber number = new ComplexNumber(2.5, -1.5);
+//        one.add(1,1);
+//        System.out.println("one.real= " + one.getReal());
+//        System.out.println("one.imaginary= " + one.getImaginary());
+//        one.subtract(number);
+//        System.out.println("one.real= " + one.getReal());
+//        System.out.println("one.imaginary= " + one.getImaginary());
+//        number.subtract(one);
+//        System.out.println("number.real= " + number.getReal());
+//        System.out.println("number.imaginary= " + number.getImaginary());
+//
+//        // ClassExercises.CircleCylinder
+//        System.out.println("-----ClassExercises.CircleCylinder-----");
+//        Circle circle = new Circle(3.75);
+//        System.out.println("circle.radius= " + circle.getRadius());
+//        System.out.println("circle.area= " + circle.getArea());
+//        Cylinder cylinder = new Cylinder(5.55, 7.25);
+//        System.out.println("cylinder.radius= " + cylinder.getRadius());
+//        System.out.println("cylinder.height= " + cylinder.getHeight());
+//        System.out.println("cylinder.area= " + cylinder.getArea());
+//        System.out.println("cylinder.volume= " + cylinder.getVolume());
+//
+//        // ClassExercises.PoolArea
+//        System.out.println("-----PoolArea-----");
+//        Rectangle rectangle = new Rectangle(5, 10);
+//        System.out.println("rectangle.width= " + rectangle.getWidth());
+//        System.out.println("rectangle.length= " + rectangle.getLength());
+//        System.out.println("rectangle.area= " + rectangle.getArea());
+//        Cuboid cuboid = new Cuboid(5, 10, 5);
+//        System.out.println("cuboid.width= " + cuboid.getWidth());
+//        System.out.println("cuboid.length= " + cuboid.getLength());
+//        System.out.println("cuboid.area= " + cuboid.getArea());
+//        System.out.println("cuboid.height= " + cuboid.getHeight());
+//        System.out.println("cuboid.volume= " + cuboid.getVolume());
 
 //        // CONTROL FLOW
 //        // ControlFlowExercises.DayOfTheWeek
